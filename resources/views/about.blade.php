eb2f58999dd66ffa0cfdc69046733ca4a7bd0d5a
arcadio1894 <joryes1894@gmail.com>
2024-07-19 12:17:35 -0500

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.appBlog')
+
+@section('header')
+    <header class="masthead" style="background-image: url({{ asset('template/assets/img/1small-about-bg.jpg') }})">
+        <div class="container position-relative px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <div class="page-heading">
+                        <h1>Nosotros</h1>
+                        <span class="subheading">¿Quienes somos?</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+@endsection
+
+@section('content')
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <p>Hola!! soy Milly, y creo que mi inicio con el mundo cinematográfico empezó cuando me regalaron mi primer VHS o desde que vi Full House, no lo recuerdo bien. Pero desde ahí cada serie, cada película se ha convertido en una nueva aventura que voy a descubrir.</p>
+                <p>Me gustan las sitcom como the Big Bang theory o la niñera, puedo ver mil veces la películas de Disney sin aburrirme, y ya hace algunos años descubrí el mundo de los viajes en el tiempo como about time u outlander. </p>
+                <p>En este espacio te compartiré mi pasión por este mundo, exploraremos desde los clásicos que marcaron nuestra infancia hasta las ultimas producciones que nos mantienen pegados a la pantalla. ¡Bienvenidos a mi rincón de cine, sitcoms y series!</p>
+                <center><p>***</p></center>
+                <p>Hoooolaaa cinemaniacos! ... Soy Remso quienes me conocen me describen como un friki de la tecnología, tal vez sea por mi profesión, pero según tengo memoria siempre me apasionó todo este mundo. Lo que no todos saben es mi pasión por las series y películas, mi género favorito es SciFic (viajes en el tiempo) y este canal es mi medio para poder expresarlo.</p>
+                <p>Lo que espero de esta comunidad es que juntos podamos compartir experiencias únicas ligadas al mundo del séptimo arte. ¿Sobre mis pelis favoritas? Back To The Future (Obviamente) , About Time, Una esposa de mentiras , Crazy & Stupid Love , El Lobo de Wall Street , Shrek 2 ... Creo que no me alcanza un solo post para comentarles todos mis gustos cinéfilos. Así que nos vemos en cada post o video para conocernos mejor</p>
+            </div>
+        </div>
+    </div>
+@endsection
